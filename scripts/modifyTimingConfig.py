203f3ac27020853b52ed3881a2faf9fd7e578ff1
Jan Schulz <jan.gerrit.schulz@cern.ch>
2026-02-02 15:54:27 +0100

    Add support for easily make timing configs

---
+++ scripts/modifyTimingConfig.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Produce and analyze HLT tracks.")
+parser.add_argument("CONFIGFILE", type=str, help="CMSSW config file to be customized.")
+parser.add_argument("CONFIGNAME", type=str, help="Name of the CMSSW config.")
+parser.add_argument("-c", "--CUSTOMIZATION", type=str, default="NONE", help="Customization of the configuration.")
+args = parser.parse_args()
+
+CONFIGFILE = args.CONFIGFILE
+CONFIGNAME = args.CONFIGNAME
+OUTPUTCONFIG = "timing/%s_config.py" % CONFIGNAME
+
+# load optional customizations
+if args.CUSTOMIZATION == "NONE":
+    CUSTOMIZATION = ""
+else:
+    with open(args.CUSTOMIZATION, "r") as f:
+        CUSTOMIZATION = f.read()
+
+# dictionary of input lines to the desired list of output lines
+inOutLines = {
+    "# customisation of the process." : [
+        "# customisation of the process.",
+        CUSTOMIZATION
+    ]
+}
+
+# open input file
+with open(CONFIGFILE,"r") as inFile:
+    # open output file
+    with open(OUTPUTCONFIG, "w") as outFile:
+        # loop through input lines and eventually replace them in the output file
+        for line in inFile:
+            if line.rstrip('\n') in inOutLines.keys():
+                for newLine in inOutLines[line.rstrip('\n')]:
+                    outFile.write(newLine + "\n")
+            else:
+                outFile.write(line)
