833560590cfd0920a2a4b65b3cb42f7a5bbbf762
Jan Schulz <jan.gerrit.schulz@cern.ch>
2026-02-16 15:16:59 +0100

    Propagate directory-enabled configs also to running scripts

--- runTrackingHarvesting.sh
+++ runTrackingHarvesting.sh
@@ -11,11 +11,14 @@ do
     esac
 done
 
+# save path of current directory
+theDir="${PWD}"
+
 # change directory, so that the output file is at the right place
 cd "results/$cutconfig/$dataset"
 
 # run the harvesting
-cmsRun ../../../config/HARVESTING_Tracking.py
+cmsRun "$theDir/config/HARVESTING_Tracking.py"
 
 # rename the output file
 mv DQM_V0001_R000000001__Global__CMSSW_X_Y_Z__RECO.root DQM_Tracking_${cutconfig##*/}.root
