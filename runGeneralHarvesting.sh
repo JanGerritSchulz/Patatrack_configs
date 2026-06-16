01eeddac6010eba0678b01cff7a07a5a6bb21a4b
Jan Schulz <jan.gerrit.schulz@cern.ch>
2026-06-16 15:46:28 +0200

    Add General validation workflow

---
+++ runGeneralHarvesting.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+### get arguments:
+#   -d dataset
+#   -c cutconfig
+while getopts d:c: flag
+do
+    case "${flag}" in
+        d) dataset=${OPTARG};;
+        c) cutconfig=${OPTARG};;
+    esac
+done
+
+# save path of current directory
+theDir="${PWD}"
+
+# change directory, so that the output file is at the right place
+cd "results/$cutconfig/$dataset"
+
+# run the harvesting
+cmsRun "$theDir/config/HARVESTING_General.py"
+
+# rename the output file
+mv DQM_V0001_R000000001__Global__CMSSW_X_Y_Z__RECO.root DQM_General_${cutconfig##*/}.root
+
+cd -
